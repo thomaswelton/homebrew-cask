7e1670e762fcc90e5db4ba5ab9bc2cc727c9dcf1
Thomas Welton <thomaswelton@me.com>
2013-12-18 10:23:35 +0000

    Create google-chrome-canary.rb

---
+++ Casks/google-chrome-canary.rb
@@ -0,0 +1,7 @@
+class GoogleChromeCanary < Cask
+  url 'https://storage.googleapis.com/chrome-canary/GoogleChromeCanary.dmg'
+  homepage 'https://www.google.co.uk/intl/en/chrome/browser/canary.html'
+  version 'stable-channel'
+  no_checksum
+  link 'Google Chrome Canary.app'
+end
